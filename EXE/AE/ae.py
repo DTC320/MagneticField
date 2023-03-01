5daa200fcc896018e7dea4d9de7ce59e86b5b858
DTC320 <dongtianchi@outlook.com>
2023-03-01 23:41:07 +0100

    Unsuccessful Demo

--- EXE/AE/ae.py
+++ EXE/AE/ae.py
@@ -1,5 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+import wandb
+
+
 
 
 class AEEncoder(nn.Module):
@@ -77,5 +83,90 @@ class AE(nn.Module):
         return x
 
 if __name__ == '__main__':
+    wandb.init(project="AE_MNIST")
+
+    _batch_size = 16
+
+    # load dataset
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomResizedCrop(size = (28, 28), scale = (0.85, 1.0))
+    ])
+
+    trainset = datasets.MNIST(root="./data/",
+                                transform=transform,
+                                train=True,
+                                download=True)
+
+    testset = datasets.MNIST(root="./data/",
+                               transform=transform,
+                               train=False,
+                               download=True)
+
+    trainloader = DataLoader(trainset,
+                             batch_size= _batch_size,
+                             shuffle= True,
+                             num_workers=2,
+                             prefetch_factor= _batch_size *2
+                             )
+    testloader = DataLoader(testset,
+                             batch_size=_batch_size,
+                             shuffle=False,
+                             num_workers=0,
+                             )
+
+    #2 Net
     net = AE(1,28,28)
-    print(net)
+    loss_fn = nn.MSELoss()
+    opt = optim.SDG(net.parameters(), lr = 0.01)
+
+
+
+    #3.train
+    total_train_samples = len(trainset)
+    total_epoch = 10
+    train_step = 0
+    test_step = 0
+    for epoch in range(total_epoch):
+        #a. train
+        net.train(True)
+        train_loss = []
+        for data in testloader:
+            images, _ = data
+
+            # forward
+            _output = net(images)
+            _loss = loss_fn(_output, images)
+            #backward
+            opt.zero_grad()
+            _loss.backward()
+            opt.step()
+
+            if train_step % 100 == 0:
+                wandb.log({'train_loss': _loss,
+                           'train_step': train_step})
+                print(f"Train {epoch + 1}/{total_epoch}  {train_step} loss:{_loss.item():.3f}")
+            train_step += 1
+
+        #b. evaluate
+        net.eval()
+        with torch.no_grad():
+            for data in testloader:
+                images, _ = data
+
+                # forward
+                _outputs = net(images)
+                _loss = loss_fn(_outputs, images)
+
+                if test_step % 10 == 0:
+                    wandb.log({'test_loss': _loss,
+                               'test_step': test_step})
+                    print(f"Test {epoch + 1}/{total_epoch}  {test_step} loss:{_loss.item():.3f}")
+                test_step += 1
+
+
+        #c. save
+        if epoch % 2 == 0:
+            torch.save(net, f'./output/ae/models/m_{epoch}.pkl')
+
+    wandb.finish()
