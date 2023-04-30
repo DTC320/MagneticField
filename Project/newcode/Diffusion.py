cff65777bc8e3e3da05d943c464a23f3e0cc6e3a
DTC320 <dongtianchi@outlook.com>
2023-04-30 09:33:57 +0200

    save

---
+++ Project/newcode/Diffusion.py
@@ -0,0 +1,140 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+import wandb
+
+
+class DiffusionModel(nn.Module):
+    def __init__(self):
+        super(DiffusionModel, self).__init__()
+
+        # 卷积层
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        # 全连接层
+        self.fc1 = nn.Sequential(
+            nn.Linear(32 * 8 * 8, 128),
+            nn.ReLU()
+        )
+
+        self.fc2 = nn.Linear(128, 3 * 32 * 32)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = torch.sigmoid(x)
+        x = x.view(-1, 3, 32, 32)
+        return x
+
+if __name__ == "__main__":
+
+    # 实例化网络
+    model = DiffusionModel()
+
+    # 定义损失函数和优化器
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.002)
+
+    # 加载数据并划分为训练集、验证集和测试集
+    data = np.load("data4D.npy")
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+    train_data, val_data = train_test_split(train_data, test_size=0.2, random_state=42)
+
+    # 将NumPy数组转换为张量
+    train_data_tensor = torch.from_numpy(train_data)
+    val_data_tensor = torch.from_numpy(val_data)
+    test_data_tensor = torch.from_numpy(test_data)
+
+    # 创建DataLoader
+    batch_size = 64
+    train_dataset = TensorDataset(train_data_tensor)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    val_dataset = TensorDataset(val_data_tensor)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    test_dataset = TensorDataset(test_data_tensor)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    reg_coeff = 0.001  # L2正则化系数
+
+
+    def calc_l2_reg(model):
+        l2_reg = 0.0
+        for param in model.parameters():
+            l2_reg += torch.norm(param, p=2)
+        return l2_reg
+
+
+    # 训练模型
+    num_epochs = 150
+    learning_rate = 0.002
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = DiffusionModel().to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    wandb.init(project="DIffusionMag",
+               config={
+                "Learning_rate": learning_rate,
+                "Architecture": "Diffusion",
+                "Dataset": "2D",
+                "Num_epochs": num_epochs,
+                "Batch_size": batch_size,
+                "Regularization coefficient" : reg_coeff
+    })
+
+    for epoch in range(num_epochs):
+        model.train()
+        epoch_loss = 0.0
+        for batch_idx, data in enumerate(train_loader):
+            inputs = data[0].to(device)
+            targets = data[0].to(device)  # 使用输入数据作为目标数据，WEIL重构输入
+
+            # 前向传播
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            l2_reg = calc_l2_reg(model)
+            total_loss = loss + reg_coeff * l2_reg
+
+            # 反向传播
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += total_loss.item()
+        wandb.log({"Epocch": epoch + 1, "Training loss": epoch_loss / len(train_loader)})
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Training Loss: {epoch_loss / len(train_loader)}')
+
+        # 验证模型
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for batch_idx, data in enumerate(val_loader):
+                inputs = data[0].to(device)
+                targets = data[0].to(device)
+
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                val_loss += loss.item()
+        wandb.log({"Epocch": epoch + 1, "Validation loss": val_loss / len(val_loader)})
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Validation Loss: {val_loss / len(val_loader)}')
+
+    wandb.finish()
---
+++ Project/newcode/VAE.py
@@ -0,0 +1,130 @@
+import numpy as np
+import wandb
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.distributions as td
+import torchvision
+from torch.utils.data import DataLoader, TensorDataset
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+
+
+class VAE(nn.Module):
+    def __init__(self, img_channels, latent_dim):
+        super(VAE, self).__init__()
+
+        # 编码器
+        self.encoder = nn.Sequential(
+            nn.Conv2d(img_channels, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+
+        # 获取编码器的输出大小
+        self.encoder_output_size = self.encoder(torch.zeros(1, img_channels, 32, 32)).shape[1]
+
+        # 计算均值和对数方差
+        self.fc_mu = nn.Linear(self.encoder_output_size, latent_dim)
+        self.fc_logvar = nn.Linear(self.encoder_output_size, latent_dim)
+
+        # 解码器
+        self.fc_decode = nn.Linear(latent_dim, self.encoder_output_size)
+        self.decoder = nn.Sequential(
+            nn.Unflatten(1, (128, 4, 4)),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, img_channels, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        h = self.encoder(x)
+        mu, logvar = self.fc_mu(h), self.fc_logvar(h)
+        z = self.reparameterize(mu, logvar)
+        x_recon = self.decoder(self.fc_decode(z))
+        return x_recon, mu, logvar
+
+
+if __name__ == "__main__":
+
+    data = np.load("data4D.npy", allow_pickle= True)
+
+    # Trainset/testset
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+
+    # 将NumPy数组转换为张量
+    train_data_tensor = torch.from_numpy(train_data)
+    test_data_tensor = torch.from_numpy(test_data)
+
+    # 创建DataLoader
+    batch_size = 64
+    train_dataset = TensorDataset(train_data_tensor)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    test_dataset = TensorDataset(test_data_tensor)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+
+    def vae_loss(x, x_recon, mu, logvar):
+        recon_loss = nn.functional.mse_loss(x_recon, x, reduction='sum') / x.size(0)
+        kl_div = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / x.size(0)
+        return recon_loss + kl_div
+
+
+    img_channels = 3
+    latent_dim = 128
+    learning_rate = 0.01
+    model = VAE(img_channels, latent_dim)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    num_epochs = 50
+    wandb.init(project="VAEMag",
+               config={
+                   "Learning_rate": learning_rate,
+                   "Architecture": "VAE",
+                   "Dataset": "2D",
+                   "Num_epochs": num_epochs,
+                   "Batch_size": batch_size,
+               })
+
+    # TRAIN
+    model.train()
+
+    for epoch in range(num_epochs):
+        train_loss = 0.0
+        for batch_idx, (data,) in enumerate(train_loader):
+            data = data.float()
+            optimizer.zero_grad()
+            recon_batch, mu, logvar = model(data)
+            loss = vae_loss(data, recon_batch, mu, logvar)
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+
+        train_loss /= len(train_loader.dataset)
+        wandb.log({"Epocch": epoch + 1, "Training loss": train_loss} )
+        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch + 1, train_loss))
+
+    model.eval()
+    test_loss = 0.0
+
+    with torch.no_grad():
+        for i, (data,) in enumerate(test_loader):
+            data = data.float()
+            recon_batch, mu, logvar = model(data)
+            test_loss += vae_loss(data, recon_batch, mu, logvar).item()
+
+    test_loss /= len(test_loader.dataset)
+    wandb.log({"Testing loss": test_loss})
+    print('====> Test set loss: {:.4f}'.format(test_loss))
